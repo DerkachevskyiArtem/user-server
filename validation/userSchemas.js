4174979e20e1ef54483e15fc9b823157dca7745b
Artem Derkachevskyi <derkachevskyiartem@gmail.com>
2025-02-10 18:58:59 +0200

    Refactor validation

--- validation/userSchemas.js
+++ validation/userSchemas.js
@@ -1,41 +1,33 @@
 const yup = require('yup');
 
+const USER_FIRSTMANE_SCHEMA = yup.string();
+const USER_LASTNAME_SCHEMA = yup.string();
+const USER_IMGSRC_SCHEMA = yup.string();
+const USER_ISMALE_SCHEMA = yup.boolean();
+const USER_EMAIL_SCHEMA = yup
+  .string()
+  .matches(/^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[a-zA-Z]{2,}$/, 'Invalid email format');
+const USER_PASSWORD_SCHEMA = yup
+  .string()
+  .matches(
+    /^[a-zA-Z0-9!@#$%^&*()_+-=]{6,}$/,
+    'Password must be at least 6 characters and contain only letters, numbers, or special characters (!@#$%^&*()_+-=)'
+  );
+
 module.exports.CREATE_USER_SCHEMA = yup.object({
-  firstName: yup.string().required(),
-  lastName: yup.string().required(),
-  imgSrc: yup.string(),
-  isMale: yup.boolean().required(),
-  email: yup
-    .string()
-    .matches(
-      /^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[a-zA-Z]{2,}$/,
-      'Invalid email format'
-    )
-    .required(),
-  password: yup
-    .string()
-    .matches(
-      /^[a-zA-Z0-9!@#$%^&*()_+-=]{6,}$/,
-      'Password must be at least 6 characters and contain only letters, numbers, or special characters (!@#$%^&*()_+-=)'
-    )
-    .required(),
+  firstName: USER_FIRSTMANE_SCHEMA.required(),
+  lastName: USER_LASTNAME_SCHEMA.required(),
+  imgSrc: USER_IMGSRC_SCHEMA,
+  isMale: USER_ISMALE_SCHEMA.required(),
+  email: USER_EMAIL_SCHEMA.required(),
+  password: USER_PASSWORD_SCHEMA.required(),
 });
 
 module.exports.UPDATE_USER_SCHEMA = yup.object({
-  firstName: yup.string(),
-  lastName: yup.string(),
-  imgSrc: yup.string(),
-  isMale: yup.boolean(),
-  email: yup
-    .string()
-    .matches(
-      /^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[a-zA-Z]{2,}$/,
-      'Invalid email format'
-    ),
-  password: yup
-    .string()
-    .matches(
-      /^[a-zA-Z0-9!@#$%^&*()_+-=]{6,}$/,
-      'Password must be at least 6 characters and contain only letters, numbers, or special characters (!@#$%^&*()_+-=)'
-    ),
+  firstName: USER_FIRSTMANE_SCHEMA,
+  lastName: USER_LASTNAME_SCHEMA,
+  imgSrc: USER_IMGSRC_SCHEMA,
+  isMale: USER_ISMALE_SCHEMA,
+  email: USER_EMAIL_SCHEMA,
+  password: USER_PASSWORD_SCHEMA,
 });
